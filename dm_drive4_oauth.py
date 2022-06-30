2c35abdeb65bbdc5286909477057ae6158fc34ca
Dave Mateer <davemateer@gmail.com>
2022-06-30 12:14:23 +0100

    auto

--- dm_drive4_oauth.py
+++ dm_drive4_oauth.py
@@ -19,26 +19,63 @@ def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
+    # token_file = 'gd-token.json'
+
+    # 1. greenbranflakes@gmail.com made on 22nd June against auto-archiver
+    # which was in testing at the time.
+    # EXPIRED!
+    # token_file = 'secrets/token-greenbranflakes-gmail.json'
+    
+    # 2. against the published project
+    # token_file = 'secrets/token-greenbranflakes-published.json'
+
+    # 3. against the published project
+    # token_file = 'secrets/token-greenbranflakes-published2.json'
+
+    # 4. against the published project
+    # dataarchingcentral@gmail.com token made by Kayleigh on 28th Jun 22
+    # shouldn't expire as on a published project
+    # token_file = 'secrets/token-dataac.json'
+
+    # 5. google workspace user dave@hms
+    # token_file = 'secrets/token-dave-hms.json'
+
+    # what the aa uses - same refresh_token as token-dataac.json
+    token_file = 'gd-token.json'
+
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # if os.path.exists('gd-token.json'):
+    if os.path.exists(token_file):
+        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
+            print('Requesting new token')
             creds.refresh(Request())
         else:
+            print('First run through so putting up login dialog')
+            # credentials.json downloaded from https://console.cloud.google.com/apis/credentials
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(token_file, 'w') as token:
+            print('Saving new token')
+            print('')
             token.write(creds.to_json())
+    else:
+        print('Token valid')
 
     try:
         service = build('drive', 'v3', credentials=creds)
+
+        # 0. About the user
+        results = service.about().get(fields="*").execute()
+        emailAddress = results['user']['emailAddress']
+        print(emailAddress)
 
         # 1. Call the Drive v3 API
         results = service.files().list(
