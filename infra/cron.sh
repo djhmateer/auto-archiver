1819803674ac1adcdf2778f898208495c95831d2
Dave Mateer <davemateer@gmail.com>
2023-01-03 11:14:25 +0000

    auto

--- infra/cron.sh
+++ infra/cron.sh
@@ -35,25 +35,25 @@ PATH=/usr/local/bin:$PATH
 # pipenv run python auto_archive.py --config config-test-hashing-2.yaml
 
 
-pipenv run python auto_archive.py --config config-aw.yaml
+#pipenv run python auto_archive.py --config config-aw.yaml
 pipenv run python auto_archive.py --config config-mw.yaml
-pipenv run python auto_archive.py --config config-eor.yaml
-pipenv run python auto_archive.py --config config-ukraine-environment.yaml
-pipenv run python auto_archive.py --config config-cir-projects.yaml
+#pipenv run python auto_archive.py --config config-eor.yaml
+#pipenv run python auto_archive.py --config config-ukraine-environment.yaml
+#pipenv run python auto_archive.py --config config-cir-projects.yaml
 
-pipenv run python auto_archive.py --config config-osr-demo.yaml
+#pipenv run python auto_archive.py --config config-osr-demo.yaml
 
-pipenv run python auto_archive.py --config config-airwars.yaml
+#pipenv run python auto_archive.py --config config-airwars.yaml
 
-pipenv run python auto_archive.py --config config-france24.yaml
+#pipenv run python auto_archive.py --config config-france24.yaml
 
-pipenv run python auto_archive.py --config config-un-demo.yaml
+#pipenv run python auto_archive.py --config config-un-demo.yaml
 
-pipenv run python auto_archive.py --config config-wagner-demo.yaml
+#pipenv run python auto_archive.py --config config-wagner-demo.yaml
 
-pipenv run python auto_archive.py --config config-amnesty-demo.yaml
+#pipenv run python auto_archive.py --config config-amnesty-demo.yaml
 
-# pipenv run python auto_archive.py --config config-fb-test.yaml
+## pipenv run python auto_archive.py --config config-fb-test.yaml
 
 
 ## cron job output is in 
