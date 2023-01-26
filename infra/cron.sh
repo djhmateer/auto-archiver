5bfdfe0b83371e996270bb1e514bfda7d8ac457c
Dave Mateer <davemateer@gmail.com>
2023-01-26 14:49:16 +0000

    auto

--- infra/cron.sh
+++ infra/cron.sh
@@ -10,24 +10,52 @@
 
 # on first build of a machine, check to make sure all secret environment files are there
 # anon.session is the last to be copied
-FILE=/home/dave/auto-archiver/anon.session
-if test -f "$FILE"; then
-    echo "$FILE exists."
-else
-     echo "secrets not all there yet, waiting for next cron run in 1 minute" >> /home/dave/log.txt 2>&1
-     exit
-fi
-
-# so only 1 instance of this will run if job lasts longer than 1 minute
-# https://askubuntu.com/a/915731/677298
-if [ $(pgrep -c "${0##*/}") -gt 1 ]; then
-     echo "Another instance of the script is running. Aborting." >> /home/dave/log.txt 2>&1
-     exit
-fi
 
 cd /home/dave/auto-archiver
-# do I need this?
 PATH=/usr/local/bin:$PATH
+
+# FILE=/home/dave/auto-archiver/anon.session
+# if test -f "$FILE"; then
+#     echo "$FILE exists."
+# else
+#      echo "secrets not all there yet, waiting for next cron run in 1 minute" >> /home/dave/log.txt 2>&1
+#      exit
+# fi
+
+# only 1 instance of this will run if job lasts longer than 1 minute
+# https://askubuntu.com/a/915731/677298
+if [ $(pgrep -c "${0##*/}") -gt 1 ]; then
+     now_human=$(date)
+     printf "\ndate: $now_human \n" >> /home/dave/log.txt 2>&1
+     # get last modified date of log file
+     # y is human readable, Y is unix epoch
+     last_modified_time=$(stat -c %Y logs/1trace.log)
+     #echo "last_modified: $last_modified_time" >> /home/dave/time_log.txt 2>&1
+
+     now=$(date +%s)
+     #echo "now: $now" >> /home/dave/time_log.txt 2>&1
+
+     difference=$(($now-last_modified_time))
+     echo "difference between 1trace.log file last modified and now: $difference seconds" >> /home/dave/log.txt 2>&1
+
+
+     TIMETOWAIT=3600
+     if (($difference > $TIMETOWAIT)); then
+          printf "\ndate: $now_human \n" >> /home/dave/kill_log.txt 2>&1
+          echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron.sh has produced no stdout!" >> /home/dave/log.txt 2>&1
+          echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron.sh has produced no stdout!" >> /home/dave/kill_log.txt 2>&1
+          pid=$(pgrep -f auto_archive_fb)
+          echo "pid is $pid" >> /home/dave/log.txt 2>&1
+          kill -9 $pid
+          echo "killed" >> /home/dave/log.txt 2>&1
+          echo "killed" >> /home/dave/kill_log.txt 2>&1
+     else
+          echo "time diff less then $TIMETOWAIT - normal control flow when the archiver is running" >> /home/dave/log.txt 2>&1
+     fi
+
+     echo "Another instance of the script is running. Aborting this run of cron.sh " >> /home/dave/log.txt 2>&1
+     exit
+fi
 
 
 # test sheets, saving to google drives with davemateer@gmail.com token
@@ -35,7 +63,7 @@ PATH=/usr/local/bin:$PATH
 # pipenv run python auto_archive.py --config config-test-hashing-2.yaml
 ## pipenv run python auto_archive.py --config config-fb-test.yaml
 
-TIME=5
+TIME=0
 pipenv run python auto_archive.py --config config-aw.yaml
 sleep $TIME
 pipenv run python auto_archive.py --config config-mw.yaml
@@ -66,6 +94,11 @@ sleep $TIME
 #pipenv run python auto_archive.py --config config-cmu-demo.yaml
 
 pipenv run python auto_archive.py --config config-aa-demo-main.yaml
+
+pipenv run python auto_archive.py --config config-rmit-demo.yaml
+sleep $TIME
+
+pipenv run python auto_archive.py --config config-bellingcat-demo.yaml
 
 ## cron job output is in 
 ## vim /home/dave/log.txt
--- infra/cron_fb.sh
+++ infra/cron_fb.sh
@@ -8,34 +8,76 @@
 
 # * * * * * dave /home/dave/auto-archiver/infra/cron_fb.sh
 
-# so only 1 instance of this will run if job lasts longer than 1 minute
+cd /home/dave/auto-archiver
+PATH=/usr/local/bin:$PATH
+
+# only used in deployment
+# FILE=/home/dave/auto-archiver/anon.session
+# if test -f "$FILE"; then
+#     echo "$FILE exists."
+# else
+#      echo "secrets not all there yet, waiting for next cron run in 1 minute" >> /home/dave/log.txt 2>&1
+#      exit
+# fi
+
+# only 1 instance of this will run if job lasts longer than 1 minute
 # https://askubuntu.com/a/915731/677298
 if [ $(pgrep -c "${0##*/}") -gt 1 ]; then
-     echo "Another instance of the script is running. Aborting." >> /home/dave/log.txt 2>&1
+     now_human=$(date)
+     printf "\ndate: $now_human \n" >> /home/dave/log.txt 2>&1
+     # get last modified date of log file
+     # y is human readable, Y is unix epoch
+     last_modified_time=$(stat -c %Y logs/1trace.log)
+     #echo "last_modified: $last_modified_time" >> /home/dave/time_log.txt 2>&1
+
+     now=$(date +%s)
+     #echo "now: $now" >> /home/dave/time_log.txt 2>&1
+
+     difference=$(($now-last_modified_time))
+     echo "difference between 1trace.log file last modified and now: $difference seconds" >> /home/dave/log.txt 2>&1
+
+
+     TIMETOWAIT=3600
+     if (($difference > $TIMETOWAIT)); then
+          printf "\ndate: $now_human \n" >> /home/dave/kill_log.txt 2>&1
+          echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron_fb.sh has produced no stdout!" >> /home/dave/log.txt 2>&1
+          echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron_fb.sh has produced no stdout!" >> /home/dave/kill_log.txt 2>&1
+          pid=$(pgrep -f auto_archive_fb)
+          echo "pid is $pid" >> /home/dave/log.txt 2>&1
+          kill -9 $pid
+          echo "killed" >> /home/dave/log.txt 2>&1
+          echo "killed" >> /home/dave/kill_log.txt 2>&1
+     else
+          echo "time diff less then $TIMETOWAIT - normal control flow when the FB archiver is running" >> /home/dave/log.txt 2>&1
+     fi
+
+     echo "Another instance of the script is running. Aborting this run of cron_fb.sh " >> /home/dave/log.txt 2>&1
      exit
 fi
 
-cd /home/dave/auto-archiver
-# do I need this?
-PATH=/usr/local/bin:$PATH
-
 
 # test sheets, saving to google drives with davemateer@gmail.com token
-pipenv run python auto_archive_fb.py --config config-test-hashing.yaml
+#pipenv run python auto_archive_fb.py --config config-test-hashing.yaml
 # pipenv run python auto_archive.py --config config-test-hashing-2.yaml
 
-
-#pipenv run python auto_archive.py --config config-aw.yaml
+TIME=0
+pipenv run python auto_archive_fb.py --config config-aw.yaml
+sleep $TIME
 
 # DM turn these on for FB
-# pipenv run python auto_archive.py --config config-mw.yaml
-# pipenv run python auto_archive.py --config config-cmu-demo.yaml
+pipenv run python auto_archive_fb.py --config config-mw.yaml
+sleep $TIME
+#pipenv run python auto_archive_fb.py --config config-cmu-demo.yaml
 
-#pipenv run python auto_archive.py --config config-eor.yaml
-#pipenv run python auto_archive.py --config config-ukraine-environment.yaml
-#pipenv run python auto_archive.py --config config-cir-projects.yaml
+pipenv run python auto_archive_fb.py --config config-eor.yaml
+sleep $TIME
 
-#pipenv run python auto_archive.py --config config-osr-demo.yaml
+pipenv run python auto_archive_fb.py --config config-ukraine-environment.yaml
+sleep $TIME
+
+#pipenv run python auto_archive_fb.py --config config-cir-projects.yaml
+
+#pipenv run python auto_archive_fb.py --config config-osr-demo.yaml
 
 #pipenv run python auto_archive.py --config config-airwars.yaml
 
@@ -50,6 +92,14 @@ pipenv run python auto_archive_fb.py --config config-test-hashing.yaml
 ## pipenv run python auto_archive.py --config config-fb-test.yaml
 
 
+
+pipenv run python auto_archive_fb.py --config config-aa-demo-main.yaml
+sleep $TIME
+
+pipenv run python auto_archive_fb.py --config config-rmit-demo.yaml
+sleep $TIME
+
+pipenv run python auto_archive_fb.py --config config-bellingcat-demo.yaml
 ## cron job output is in 
 ## vim /home/dave/log.txt
 
