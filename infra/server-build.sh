2e1ec191f3c0c24b29cf02fe9cf111d235ecfc1b
Dave Mateer <davemateer@gmail.com>
2023-11-18 11:44:42 +0000

    auto

--- infra/server-build.sh
+++ infra/server-build.sh
@@ -108,7 +108,11 @@ sudo apt install ffmpeg -y
 cd ~
 sudo add-apt-repository ppa:mozillateam/ppa -y
 
-echo ' Package: * Pin: release o=LP-PPA-mozillateam Pin-Priority: 1001 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@cirautoarchiver432.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@cirautoarchiver38.westeurope.cloudapp.azure.com
 
