435886b1e70194f32068ea7d34afd2fdc87e8eaa
Dave Mateer <davemateer@gmail.com>
2023-11-25 05:40:31 +0000

    auto

--- src/auto_archiver/enrichers/__init__.py
+++ src/auto_archiver/enrichers/__init__.py
@@ -6,5 +6,5 @@ from .thumbnail_enricher import ThumbnailEnricher
 from .wacz_enricher import WaczArchiverEnricher
 from .whisper_enricher import WhisperEnricher
 # DM commented out as problem on server not installing
-from .pdq_hash_enricher import PdqHashEnricher
+# from .pdq_hash_enricher import PdqHashEnricher
 from .metadata_enricher import MetadataEnricher
