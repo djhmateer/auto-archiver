b695f685f8e30e0a92e5f6ee68cd7064f0127a69
Dave Mateer <davemateer@gmail.com>
2024-12-03 11:20:16 +0000

    auto

--- infra/cron_5.sh
+++ infra/cron_5.sh
@@ -15,7 +15,8 @@ PATH=/usr/local/bin:$PATH
 # https://askubuntu.com/a/915731/677298
 if [ $(pgrep -c "${0##*/}") -gt 1 ]; then
      now_human=$(date)
-     printf "\ndate: $now_human \n" >> /home/dave/log.txt 2>&1
+     # printf "\ndate: $now_human \n" >> /home/dave/log.txt 2>&1
+
      # get last modified date of log file
      # y is human readable, Y is unix epoch
      last_modified_time=$(stat -c %Y logs/1trace.log)
@@ -25,27 +26,30 @@ if [ $(pgrep -c "${0##*/}") -gt 1 ]; then
      #echo "now: $now" >> /home/dave/time_log.txt 2>&1
 
      difference=$(($now-last_modified_time))
-     echo "difference between 1trace.log file last modified and now: $difference seconds" >> /home/dave/log.txt 2>&1
+     # echo "difference between 1trace.log file last modified and now: $difference seconds" >> /home/dave/log.txt 2>&1
 
 
      # NOTE HAVE DISABLED THE KILL FOR NOW
      TIMETOWAIT=3600
      if (($difference > $TIMETOWAIT)); then
-          printf "\ndate: $now_human \n" >> /home/dave/kill_log.txt 2>&1
-          echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron.sh has produced no stdout!" >> /home/dave/log.txt 2>&1
-          echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron.sh has produced no stdout!" >> /home/dave/kill_log.txt 2>&1
-          # pid=$(pgrep -f auto_archive_fb)
-          echo "pid is $pid" >> /home/dave/log.txt 2>&1
-          # kill -9 $pid
-          echo "killed" >> /home/dave/log.txt 2>&1
-          echo "killed" >> /home/dave/kill_log.txt 2>&1
-          # as there are firefox processes which need to be killed
+          # printf "\ndate: $now_human \n" >> /home/dave/kill_log.txt 2>&1
+          # echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron.sh has produced no stdout!" >> /home/dave/log.txt 2>&1
+          # echo "time diff greater then $TIMETOWAIT seconds - kill the process as cron.sh has produced no stdout!" >> /home/dave/kill_log.txt 2>&1
+          # # pid=$(pgrep -f auto_archive_fb)
+          # echo "pid is $pid" >> /home/dave/log.txt 2>&1
+          # # kill -9 $pid
+          # echo "killed" >> /home/dave/log.txt 2>&1
+          # echo "killed" >> /home/dave/kill_log.txt 2>&1
+          # # as there are firefox processes which need to be killed
           # sudo reboot
+          echo "problem - probably a stuck process"
      else
-          echo "time diff less then $TIMETOWAIT - normal control flow when the archiver is running" >> /home/dave/log.txt 2>&1
+          # echo "time diff less then $TIMETOWAIT - normal control flow when the archiver is running" >> /home/dave/log.txt 2>&1
+          echo "normal control flow when archiver is running"
      fi
 
-     echo "Another instance of the script is running. Aborting this run of cron.sh " >> /home/dave/log.txt 2>&1
+     # echo "Another instance of the script is running. Aborting this run of cron.sh " >> /home/dave/log.txt 2>&1
+     echo "Another instance of the script is running. Aborting this run of cron.sh "
      exit
 fi
 
@@ -61,29 +65,3 @@ cd /home/dave/auto-archiver
 
 pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-domain-eor.yaml
 sleep $TIME
-
-# pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-domain-cir-sahel.yaml
-# sleep $TIME
-
-#pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-domain-cir-sudan.yaml
-#sleep $TIME
-
-#pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-domain-aw.yaml
-#sleep $TIME
-
-#pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-domain-eor-grc.yaml
-#sleep $TIME
-
-#pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-domain-mw.yaml
-#sleep $TIME
-
-#pipenv run python -m src.auto_archiver --config secrets/orchestration-cir-projects.yaml
-#sleep $TIME
-
-
-## cron job output is in 
-## vim /home/dave/log.txt
-
-# to stop cron job comment out in /etc/cron.d
-# then reload
-# sudo service cron reload
