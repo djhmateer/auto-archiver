fcc41360a3cc155382ea4a2acf13104368ec0a24
Dave Mateer <davemateer@gmail.com>
2025-10-07 09:55:06 +0100

    patch in anitbot user datra

--- src/auto_archiver/screenshot.py
+++ src/auto_archiver/screenshot.py
@@ -2,11 +2,13 @@ from seleniumbase import SB
 
 # Load the extension we created
     # extension_dir="secrets/twitter_extension"
+
+    #headless2-Tue needed on prod otherwise errors
 with SB(
     uc=True,
     headless2=True,
     xvfb=True,
-    # user_data_dir="secrets/antibot_user_data"
+    user_data_dir="secrets/antibot_user_data"
 ) as sb:
     sb.uc_open_with_reconnect("https://x.com/TangoBatDraws/status/1776776952298500381", 4)
     sb.save_screenshot("twitter_with_extension.png")
