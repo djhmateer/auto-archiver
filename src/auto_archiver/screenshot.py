deb8ca33d2b86c59d14fda4ed89c65c8ddd593b2
Dave Mateer <davemateer@gmail.com>
2025-10-07 06:57:29 +0100

    no prt

--- src/auto_archiver/screenshot.py
+++ src/auto_archiver/screenshot.py
@@ -4,12 +4,24 @@ import random
 # Use a random port to avoid conflicts
 port = random.randint(9300, 9400)
 
+# this works
+# with SB(
+#     uc=True,
+#     headless2=True,
+#     xvfb=True,
+#     incognito=True,
+#     chromium_arg=f"--remote-debugging-port={port},--no-sandbox,--disable-dev-shm-usage,--disable-gpu"
+# ) as sb:
+#     sb.uc_open_with_reconnect("https://example.org", 4)
+#     sb.save_screenshot("example.png")
+#     print("Screenshot saved to example.png")
+
 with SB(
     uc=True,
     headless2=True,
     xvfb=True,
     incognito=True,
-    chromium_arg=f"--remote-debugging-port={port},--no-sandbox,--disable-dev-shm-usage,--disable-gpu"
+    chromium_arg=f"--no-sandbox,--disable-dev-shm-usage,--disable-gpu"
 ) as sb:
     sb.uc_open_with_reconnect("https://example.org", 4)
     sb.save_screenshot("example.png")
