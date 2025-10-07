be69a32ac09c259cd814a19f43f245c327328ea8
Dave Mateer <davemateer@gmail.com>
2025-10-07 07:00:14 +0100

    asdf

--- src/auto_archiver/screenshot.py
+++ src/auto_archiver/screenshot.py
@@ -28,12 +28,24 @@ port = random.randint(9300, 9400)
 #     sb.save_screenshot("example.png")
 #     print("Screenshot saved to example.png")
 
+# works
+# with SB(
+#     uc=True,
+#     headless2=True,
+#     xvfb=True,
+#     incognito=True,
+#     chromium_arg=f"--disable-dev-shm-usage,--disable-gpu"
+# ) as sb:
+#     sb.uc_open_with_reconnect("https://example.org", 4)
+#     sb.save_screenshot("example.png")
+#     print("Screenshot saved to example.png")
+
 with SB(
     uc=True,
     headless2=True,
     xvfb=True,
     incognito=True,
-    chromium_arg=f"--disable-dev-shm-usage,--disable-gpu"
+    chromium_arg=f"--disable-gpu"
 ) as sb:
     sb.uc_open_with_reconnect("https://example.org", 4)
     sb.save_screenshot("example.png")
