a2068c42e21ed708291c9c2bf878283048036ed7
Dave Mateer <davemateer@gmail.com>
2026-09-09 12:24:46 +0100

    fix: Comment out orchestration for glan-sheet3 in cron script

--- scripts/cron_11.sh
+++ scripts/cron_11.sh
@@ -68,7 +68,7 @@ poetry run python src/auto_archiver --config secrets/orchestration-aa-demo-main.
 
 poetry run python src/auto_archiver --config secrets/orchestration-glan-sheet1.yaml
 
-poetry run python src/auto_archiver --config secrets/orchestration-glan-sheet3.yaml
+# poetry run python src/auto_archiver --config secrets/orchestration-glan-sheet3.yaml
 
 #poetry run python src/auto_archiver --config secrets/orchestration-aa-pogo.yaml
 sleep $TIME
